f3c88c4f1227c9d545bc20eb498d49b9450802c4
Joshua Willett <jwillet1@gmail.com>
2017-08-02 20:49:30 -0400

    removed eslint in favor of prettier

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const keys = require('./config/keys');
+const keys = require("./config/keys");
 
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    secret: keys.googleClientSecret,
-  }),
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      secret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
 );
 
 const PORT = process.env.PORT || 5000;
