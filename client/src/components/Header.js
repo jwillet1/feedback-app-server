6effb444094f4857972f883a818e74dd71ff1c26
Joshua Willett <jwillet1@gmail.com>
2017-08-10 19:22:08 -0400

    Finish stripe setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,13 +16,13 @@ class Header extends Component {
         );
       default:
         return [
-          <li>
+          <li key="1">
             <Payments />
           </li>,
-          <li>
-            <a href="#">Credits: 5</a>
+          <li key="2" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
           </li>,
-          <li>
+          <li key="3">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
