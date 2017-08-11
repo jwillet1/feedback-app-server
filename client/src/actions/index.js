b23673fb2382174de6848b92efb1cf3ba51c6b6f
Joshua Willett <jwillet1@gmail.com>
2017-08-10 21:06:18 -0400

    continue bug checking on production

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-import * as types from './types.js';
+import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
-  dispatch({ type: types.FETCH_USER, payload: res.data });
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
-  dispatch({ type: types.FETCH_USER, payload: res.data });
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
